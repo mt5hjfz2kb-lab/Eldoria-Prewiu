e3b47bf05ad9b68703bc47e78c3eb1c1ca542535
mt5hjfz2kb-lab <mt5hjfz2kb@privaterelay.appleid.com>
2026-09-22 18:50:30 +0200

    Record tester test.2 frozen snapshot

--- tester-v0265/tester-config.js
+++ tester-v0265/tester-config.js
@@ -2,7 +2,7 @@ window.ELDORIA_TESTER_CONFIG={
   buildName:'Eldoria Closed Playtest T1',
   version:'0.26.5-test.2',
   sourceCommit:'a6bf164180083938adaceffff293b5709e27269a',
-  frozenCommit:'',
+  frozenCommit:'4ba5ec37dd7d0e3641050936e411fb9befcdce58',
   endpoint:'https://eavaxwpxxcrwibooiimb.supabase.co',
   anonKey:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVhdmF4d3B4eGNyd2lib29paW1iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3OTAwNzc2NTQsImV4cCI6MjEwNTY1MzY1NH0.7oYVNF4TTf3sd2BTMOvCF4s8Q8y5DjZlCUWWP70Om6k',
   table:'eldoria_feedback'
