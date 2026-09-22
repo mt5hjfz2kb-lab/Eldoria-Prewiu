fb7c5191c67063d160a31a8d39404c16a3b36698
mt5hjfz2kb-lab <mt5hjfz2kb@privaterelay.appleid.com>
2026-09-22 15:20:30 +0200

    Add tester build configuration

---
+++ tester-v0265/tester-config.js
@@ -0,0 +1,9 @@
+window.ELDORIA_TESTER_CONFIG={
+  buildName:'Eldoria Closed Playtest T1',
+  version:'0.26.5-test.1',
+  sourceCommit:'67f6ff8fd10a01a428d29b22c678f51e5a7225f1',
+  frozenCommit:'',
+  endpoint:'',
+  anonKey:'',
+  table:'eldoria_feedback'
+};
