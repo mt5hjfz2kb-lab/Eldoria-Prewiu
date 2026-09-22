8afa2d696ec9af126abbc9dae5671c0931e510fa
mt5hjfz2kb-lab <mt5hjfz2kb@privaterelay.appleid.com>
2026-09-22 15:34:54 +0200

    Configure tester feedback persistence

--- tester-v0265/tester-config.js
+++ tester-v0265/tester-config.js
@@ -3,7 +3,7 @@ window.ELDORIA_TESTER_CONFIG={
   version:'0.26.5-test.1',
   sourceCommit:'67f6ff8fd10a01a428d29b22c678f51e5a7225f1',
   frozenCommit:'',
-  endpoint:'',
-  anonKey:'',
+  endpoint:'https://eavaxwpxxcrwibooiimb.supabase.co',
+  anonKey:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVhdmF4d3B4eGNyd2lib29paW1iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3OTAwNzc2NTQsImV4cCI6MjEwNTY1MzY1NH0.7oYVNF4TTf3sd2BTMOvCF4s8Q8y5DjZlCUWWP70Om6k',
   table:'eldoria_feedback'
 };
